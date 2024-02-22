38952ad53f5459576ac752718753d04aab94f5f1
lokesh pandey <luvpnd@gmail.com>
2024-02-22 12:58:41 +0530

    more changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const VerticleNavbarComponent = document.querySelector("#header_menu");
 const NavbarButtonComponent = document.querySelector("#slide-menu");
 const NameContainer = document.querySelector(".text-container");
 const MoveToTopButon = document.querySelector("#move-to-top");
+const LinktoMailButton = document.querySelector("#mail-link");
 
 function handleToggleNavbar() {
   VerticleNavbarComponent.classList.toggle("slide-down");
@@ -27,3 +28,12 @@ function handleMoveToTop() {
 }
 MoveToTopButon.addEventListener("click", handleMoveToTop);
 NavbarButtonComponent.addEventListener("click", handleToggleNavbar);
+
+LinktoMailButton.addEventListener("click", function (event) {
+  let choice = window.confirm(
+    "This will open your Gmail application. Do you want to proceed ?"
+  );
+  if (choice) {
+    LinktoMailButton.setAttribute("href", "mailto:luvpnd@gmail.com");
+  }
+});
